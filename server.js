c24c923bdf44f34f427d3b954f2ab914e34bd398
immber <jessiekrushing@gmail.com>
2025-02-13 10:37:57 -0700

    added form to collect handle

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ const app = express();
 // serve files from the public directory
 app.use(express.static('public'));
 
+// allow urlencoded payloads
+app.use(express.urlencoded({ extended: false }));
+
 // start the express web server listening on 8080
 app.listen(8080, () => {
   console.log('listening on 8080');
 });
 
-// serve the homepage
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 // handle login btn click
 app.post('/login', (req, res) => {
     console.log('Add oauth to bsky')
+    const handle = req.body.handle;
+    console.log(handle);
     res.sendStatus(200);
+});
+
+// serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
